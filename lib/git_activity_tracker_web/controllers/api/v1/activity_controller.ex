7d1aa84a2f3e6616f8a2d93d3b5515c069b88a40
Medo <medo.a.haleem@gmail.com>
2020-04-09 15:14:15 +0200

    FEAT: added the ability to save the commit in commit push payload

--- lib/git_activity_tracker_web/controllers/api/v1/activity_controller.ex
+++ lib/git_activity_tracker_web/controllers/api/v1/activity_controller.ex
@@ -2,8 +2,17 @@ defmodule GitActivityTrackerWeb.Api.V1.ActivityController do
   use GitActivityTrackerWeb, :controller
 
   alias GitActivityTracker.Activity
+  alias GitActivityTracker.Authors
 
   def create(conn, %{"pull_request" => %{"commits" => commits}, "repository" => repository}) do
+    save_commits_in_db(conn, commits, repository)
+  end
+
+  def create(conn, %{"commits" => commits, "repository" => repository}) do
+    save_commits_in_db(conn, commits, repository)
+  end
+
+  defp save_commits_in_db(conn, commits, repository) do
     with commit_repository <-
            Activity.find_or_create_repository(repository),
          activites <- Activity.save_commits(commit_repository, commits) do
@@ -13,6 +22,4 @@ defmodule GitActivityTrackerWeb.Api.V1.ActivityController do
       |> render("index.json", activites: activites)
     end
   end
-
-
 end
